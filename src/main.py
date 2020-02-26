02730e4b12b6f3473bf1f28fa0c555a682573928
cjawesomest <cjcalv@gmail.com>
2020-02-25 21:24:09 -0500

    Added more data scrapes and updated plot output

--- src/main.py
+++ src/main.py
@@ -13,10 +13,13 @@ if __name__ == '__main__':
 
     output_flag: bool = True
     two_queries_flag: bool = False
+    if not two_queries_flag:
+        search_term_2 = ''
     import_flag: bool = True
-    search_term_1 = 'Marvel Cinematic Universe'
+    # search_term_1 = 'Marvel Cinematic Universe'
+    search_term_1 = 'Planet of the Apes'
     search_term_2 = 'High School Musical'
-    # search_term_1 = 'Star wars'
+    # search_term_1 = 'Star Wars'
     # search_term_2 = 'Lord of the Rings'
 
     data_name_1 = search_term_1.strip().lower().replace(" ", "")
@@ -77,28 +80,7 @@ if __name__ == '__main__':
             if coeffs_2[performer_node] > max_cc_2:
                 max_cc_2 = coeffs_2[performer_node]
 
-    # time_1 = datetime.now()
-    # [links_1, names_1] = findMovies(search_term_1)
-    # [link_to_movie_1, link_to_cast_1, performer_to_name_1, performer_to_movies_1] = aggregateInfo(links_1, names_1)
-    #Save output of search so we don't have to do it again
-    # with open("./../dat/"+data_name_1+".dat", 'wb') as file:
-    #     obj_out.dump([link_to_movie_1, link_to_cast_1, performer_to_name_1, performer_to_movies_1], file)
-
-
-    # time_2 = datetime.now()
-    # [links_2, names_2] = findMovies(search_term_2)
-    # [link_to_movie_2, link_to_cast_2, performer_to_name_2, performer_to_movies_2] = aggregateInfo(links_2, names_2)
-    # with open("./../dat/"+data_name_2+".dat", 'wb') as file:
-    #     obj_out.dump([link_to_movie_2, link_to_cast_2, performer_to_name_2, performer_to_movies_2], file)
-    # [graph_2, colors_2] = createNetworkGraph(link_to_movie_2, link_to_cast_2, performer_to_name_2, performer_to_movies_2)
-    # acc_2 = nx.average_clustering(graph_2) #Clustering Coefficient
-    # coeffs_2 = nx.clustering(graph_2)
-    # max_cc_2 = 0
-    # num_nodes_2 = 0
-    # for performer_node in coeffs_2.keys():
-    #     num_nodes_2 += 1
-    #     if coeffs_2[performer_node] > max_cc_2:
-    #         max_cc_2 = coeffs_2[performer_node]
+    # Plot the data
 
     colormap = plt.cm.get_cmap('plasma')
     fig = plt.figure()
@@ -113,7 +95,12 @@ if __name__ == '__main__':
     props_1 = '\n'.join(("Avg Cluster Coeff: " + str(round(acc_1,3)),
                            "Max Cluster Coeff: " + str(round(max_cc_1,3)),
                            "Num Nodes: " + str(num_nodes_1)))
-    ax.text(0.05, 0.05, props_1, transform=ax.transAxes, va='bottom', ha='left', bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.8))
+    if not two_queries_flag:
+        ax.annotate(props_1, xy=(0.05, -0.1), xycoords=("data", "axes fraction"), va='bottom', ha='center',
+                  bbox=dict(boxstyle='round4', facecolor='orange', alpha=0.8))
+    else:
+        ax.annotate(props_1, xy=(0.05, -0.3), xycoords=("data", "axes fraction"), va='bottom', ha='center',
+                    bbox=dict(boxstyle='round4', facecolor='orange', alpha=0.8))
     sm = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=min(colors_1), vmax=max(colors_1)))
     sm.set_array([])
     cbar = plt.colorbar(sm, shrink=0.75)
@@ -128,7 +115,9 @@ if __name__ == '__main__':
         props_2 = '\n'.join(("Avg Cluster Coeff: "+str(round(acc_2, 3)),
                                "Max Cluster Coeff: "+str(round(max_cc_2, 3)),
                                "Num Nodes: "+str(num_nodes_2)))
-        ax.text(0.05, 0.05, props_2, transform=ax.transAxes, va='bottom', ha='left', bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.8))
+        ax.annotate(props_2, xy=(0.05, -0.3), xycoords=("data", "axes fraction"), va='bottom', ha='center',
+                    bbox=dict(boxstyle='round4', facecolor='orange', alpha=0.8))
+        # plt.gcf().text(0.05, 0.05, props_2, transform=ax.transAxes, va='bottom', ha='left', bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.8))
         sm = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=min(colors_2), vmax=max(colors_2)))
         sm.set_array([])
         cbar = plt.colorbar(sm, shrink=0.75)
